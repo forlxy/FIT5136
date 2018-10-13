e2c286f5c50404edd9487ec1d2eab777990d349a
Weijia <mappple@qq.com>
2018-10-13 14:00:38 +1100

    Add readFile writeFile function

--- FinalProject/Order.java
+++ FinalProject/Order.java
@@ -24,7 +24,7 @@ public class Order {
                 ", products=" + products +
                 ", time=" + time +
                 ", paidPrice=" + paidPrice +
-                '}';
+                "}";
     }
 
     @Override
--- FinalProject/RegisteredController.java
+++ FinalProject/RegisteredController.java
@@ -1,6 +1,12 @@
-
+import java.io.FileNotFoundException;
 
 public class RegisteredController {
+    public static void readFile() throws FileNotFoundException {
+        Registered.readFile();
+    }
 
+    public static void writeFile() throws FileNotFoundException {
+        Registered.writeFile();
+    }
 
 }
